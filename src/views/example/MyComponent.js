0c6615b2e29fab56ccec144b8cbe6725038648db
phuongda <phuongdaps03397@gmail.com>
2021-10-17 22:22:43 +0700

    Lession 7 - DOM Events

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -13,12 +13,20 @@ class MyComponent extends React.Component {
         })
     }
 
+    handleClickButton = () => {
+        console.log('Click button');
+        alert('Click me!');
+    }
+
     render() {
         return (
             <>
                 <input type="test" value={this.state.name} onChange={(event) => this.handleOnChangeName(event)}></input>
                 <div className="box-children-1">Hello! My name is {this.state.name}, i'm {this.state['age']}</div>
                 <div className="box-children-2">this is My Component</div>
+                <div className="box-btn">
+                    <button onClick={() => this.handleClickButton()}>Click me!</button>
+                </div>
             </>
         )
     }
