6e68aa2bc9dbec8dad1cac9275a2a2ff6f22565f
phuongda <phuongdaps03397@gmail.com>
2021-10-17 20:36:08 +0700

    Lession 5 - State

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Won Su',
+        age: 27
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
         return (
-            <div>My Component</div>
+            <>
+                <input type="test" value={this.state.name} onChange={(event) => this.handleOnChangeName(event)}></input>
+                <div className="box-children-1">Hello! My name is {this.state.name}, i'm {this.state['age']}</div>
+                <div className="box-children-2">this is My Component</div>
+            </>
         )
     }
 }
