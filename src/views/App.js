884b77c7f68029c67123e464f92c025da6bf8f3a
phuongda <phuongdaps03397@gmail.com>
2021-11-03 23:44:38 +0700

    Lession 22 - React Routers

--- src/views/App.js
+++ src/views/App.js
@@ -1,33 +1,46 @@
 import logo from './logo.svg';
 import './App.scss';
-import ListTodo from './todos/ListTodo.js';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Nav from './navigation/Nav';
+import Home from './example/Home';
+import ListTodo from './todos/ListTodo.js';
+import MyComponent from './example/MyComponent.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Simple Todo App With Me
-        </p>
-        <ListTodo />
-      </header>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      {/* Same as */}
-      <ToastContainer />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+          <img src={logo} className="App-logo" alt="logo" />
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/todo">
+              <ListTodo />
+            </Route>
+            <Route path="/about">
+              <MyComponent />
+            </Route>
+          </Switch>
+        </header>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <ToastContainer />
+      </div>
+    </Router>
   );
 }
 
--- src/views/todos/ListTodo.js
+++ src/views/todos/ListTodo.js
@@ -75,37 +75,42 @@ class ListTodo extends React.Component {
         let isEmptyObject = Object.keys(editTodo).length === 0;
 
         return (
-            <div className="list-todo-container">
-                <AddTodo addNewTodo={this.handleAddNewTodo} />
-                <div className="list-todo-content">
-                    {
-                        listTodos && listTodos.length > 0 &&
-                        listTodos.map((item, index) => {
-                            return (
-                                <div key={item.id} className="todo-child">
-                                    {isEmptyObject ?
-                                        <span>{index + 1}. {item.title}</span>
-                                        :
-                                        <>
-                                            {editTodo.id === item.id ?
-                                                <span>
-                                                    {index + 1}. <input value={editTodo.title} onChange={(event) => this.handleChangeTitle(event)} />
-                                                </span>
-                                                :
-                                                <span>{index + 1}. {item.title}</span>
-                                            }
-                                        </>
-                                    }
-                                    <button onClick={() => this.handleEditTodo(item)}>
-                                        {!isEmptyObject && editTodo.id === item.id ? 'Save' : 'Edit'}
-                                    </button>
-                                    <button onClick={() => this.handleDeleteTodo(item)}>Delete</button>
-                                </div>
-                            )
-                        })
-                    }
+            <>
+                <p>
+                    Simple Todo App With Me
+                </p>
+                <div className="list-todo-container">
+                    <AddTodo addNewTodo={this.handleAddNewTodo} />
+                    <div className="list-todo-content">
+                        {
+                            listTodos && listTodos.length > 0 &&
+                            listTodos.map((item, index) => {
+                                return (
+                                    <div key={item.id} className="todo-child">
+                                        {isEmptyObject ?
+                                            <span>{index + 1}. {item.title}</span>
+                                            :
+                                            <>
+                                                {editTodo.id === item.id ?
+                                                    <span>
+                                                        {index + 1}. <input value={editTodo.title} onChange={(event) => this.handleChangeTitle(event)} />
+                                                    </span>
+                                                    :
+                                                    <span>{index + 1}. {item.title}</span>
+                                                }
+                                            </>
+                                        }
+                                        <button onClick={() => this.handleEditTodo(item)}>
+                                            {!isEmptyObject && editTodo.id === item.id ? 'Save' : 'Edit'}
+                                        </button>
+                                        <button onClick={() => this.handleDeleteTodo(item)}>Delete</button>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
